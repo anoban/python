4e22cf8f39c2e1219f44ee111bcab0dda71673db
anoban <2016sb117@univ.jfn.ac.lk>
2023-11-05 09:32:21 +0530

    modified:   Huffman.ipynb 	new file:   crc.py 	new file:   png.ipynb 	new file:   png_simpsons_76695.png

---
+++ crc.py
@@ -0,0 +1,16 @@
+
+
+def create_crc32_lookuptable() ->  list[int]:
+    
+    """
+    
+    """
+    
+    crc_table: list[int] = list(range(256))
+    for i in range(256):
+        for _ in range(8):
+            if (crc_table[i] & 0x1 ) == 0:
+                crc_table[i] >>= 1
+            else:
+                crc_table[i] = 0xEDB88320 ^ (crc_table[i] >> 1)
+    return crc_table
