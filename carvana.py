a4533ab9c4547bb8584d18400a4e220ea7c35fa6
anoban <2016sb117@univ.jfn.ac.lk>
2025-07-23 18:06:19 +1000

    modified:   .gitignore 	new file:   carvana.ipynb 	modified:   carvana.py

--- carvana.py
+++ carvana.py
@@ -1,10 +1,10 @@
-# Parts of the U-Net model
-
 from typing import override
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+# credits https://github.com/milesial/Pytorch-UNet?tab=readme-ov-file
+from torch.nn.functional import pad
 
 
 class DoubleConv(nn.Module):
@@ -12,43 +12,49 @@ class DoubleConv(nn.Module):
     (convolution => [BN] => ReLU) * 2
     """
 
-    def __init__(self, in_channels: int, out_channels: int, mid_channels: int | None = None):
-        super().__init__()
+    def __init__(self, in_channels: int, out_channels: int, mid_channels: int | None = None) -> None:
+        super(DoubleConv, self).__init__()  # type: ignore
+
         if not mid_channels:
             mid_channels = out_channels
+
         self.double_conv = nn.Sequential(
+            # first set
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
+            # second set
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         )
 
     @override
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.double_conv(x)
 
 
 class Down(nn.Module):
     """
-    Downscaling with maxpool then double conv
+    Downscaling with maxpool then double convolution
     """
 
-    def __init__(self, in_channels: int, out_channels: int):
-        super().__init__()
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super(Down, self).__init__()  # type: ignore
         self.maxpool_conv = nn.Sequential(nn.MaxPool2d(2), DoubleConv(in_channels, out_channels))
 
     @override
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.maxpool_conv(x)
 
 
 class Up(nn.Module):
-    """Upscaling then double conv"""
+    """
+    Upscaling then double convolution
+    """
 
-    def __init__(self, in_channels: int, out_channels: int, bilinear: bool = True):
-        super().__init__()
+    def __init__(self, in_channels: int, out_channels: int, bilinear: bool = True) -> None:
+        super(Up, self).__init__()  # type: ignore
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
@@ -59,13 +65,13 @@ class Up(nn.Module):
             self.conv = DoubleConv(in_channels, out_channels)
 
     @override
-    def forward(self, x1: torch.Tensor, x2: torch.Tensor):
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
         x1 = self.up(x1)
         # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        x1 = pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
         # if you have padding issues, see
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
@@ -74,31 +80,30 @@ class Up(nn.Module):
 
 
 class OutConv(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
-        super(OutConv, self).__init__()
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super(OutConv, self).__init__()  # type: ignore
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     @override
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.conv(x)
 
 
 class UNet(nn.Module):
-    """
-    Full assembly of the parts to form the complete network
-    """
+    """ """
 
-    def __init__(self, n_channels: int, n_classes: int, bilinear: bool = False):
-        super(UNet, self).__init__()
+    def __init__(self, n_channels: int, n_classes: int, bilinear: bool = False) -> None:
+        super(UNet, self).__init__()  # type: ignore
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
+        factor: int = 2 if bilinear else 1
 
         self.inc = DoubleConv(n_channels, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
-        factor = 2 if bilinear else 1
+
         self.down4 = Down(512, 1024 // factor)
         self.up1 = Up(1024, 512 // factor, bilinear)
         self.up2 = Up(512, 256 // factor, bilinear)
@@ -120,7 +125,7 @@ class UNet(nn.Module):
         logits = self.outc(x)
         return logits
 
-    def use_checkpointing(self):
+    def use_checkpointing(self) -> None:
         self.inc = torch.utils.checkpoint(self.inc)
         self.down1 = torch.utils.checkpoint(self.down1)
         self.down2 = torch.utils.checkpoint(self.down2)
@@ -133,5 +138,11 @@ class UNet(nn.Module):
         self.outc = torch.utils.checkpoint(self.outc)
 
 
-def main() -> None:
-    pass
+def main(model_path: str, images_path: str) -> None:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = UNet(n_channels=3, n_classes=1)
+    model.to(device=device)
+
+
+if __name__ == r"__main__":
+    main()
